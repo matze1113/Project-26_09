6f2ccbf411caa2a2a23d178abee42cfaf0b48e1d
ARBEITSLAPTOP\popow <popow80@web.de>
2023-09-26 15:37:29 +0200

    3 Änderung Benutzer hinzugefügt

--- Todo/app2.js
+++ Todo/app2.js
@@ -1,86 +1,105 @@
-class AufgabenManager {
-    constructor() {
-      this.aufgaben = [];
-    }
+const users = [
+    { email: 'kort@fn.de', password: '1234' },
+    { email: 'bartlomiej.idzinski@docc.techstarter.de', password: '1234' },
+    { email: 'ingo.neubert@docc.techstarter.de', password: '1234' },
+    { email: 'mathias.popow@docc.techstarter.de', password: '1234' }
+  ]; // Array zum Speichern der Benutzer
   
-    aufgabe_hinzufuegen(aufgabe) {
-      this.aufgaben.push(aufgabe);
-      this.speichern();
-    }
-  
-    speichern() {
-      // Hier fügst du den Code zum Speichern der Aufgaben ein
-      // Zum Beispiel könntest du sie in den lokalen Speicher des Browsers speichern oder an einen Server senden
-    }
-  }
-  
-  const manager = new AufgabenManager();
-  manager.aufgabe_hinzufuegen("Aufgabe 1");
-  manager.aufgabe_hinzufuegen("Aufgabe 2");
-  
-  const users = []; // Array zum Speichern der Benutzer
+  const tasks = []; // Array zum Speichern der Aufgaben
   
   document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
+    event.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
   
-    // Benutzer hinzufügen
-    let user = {
-      email: 'dabeiisalles@web.de',
-      password: '1234'
-    };
-    users.push(user);
+    // Überprüfen der Benutzeranmeldeinformationen
+    let isValidUser = users.some(function(user) {
+      return user.email === username && user.password === password;
+    });
   
-    // Überprüfen der Benutzeranmeldeinformationen
-    let isValidUser = users.some(function(user) {
-      return user.email === username && user.password === password;
-    });
-  
-    if (isValidUser) {
-      document.getElementById('login-form').style.display = 'none';
-      document.getElementById('app-container').style.display = 'block';
-    } else {
-      alert('Falscher Benutzername oder Passwort!');
-    }
+    // Anmeldeformular wird ausgeblendet und Anwendungsbereich angezeigt
+    if (isValidUser) {
+      document.getElementById('login-form').style.display = 'none';
+      document.getElementById('app-container').style.display = 'block';
+    } else {
+      alert('Falscher Benutzername oder Passwort!');
+    }
   });
   
+  // Funktion zum Vergleichen von Datumszeichenfolgen im Format "YYYY-MM-DD"
+  function compareDueDates(a, b) {
+    const dateA = new Date(a.dueDate);
+    const dateB = new Date(b.dueDate);
+  
+    // Verwenden von getTime(), um den Vergleich in Millisekunden durchzuführen
+    if (dateA.getTime() < dateB.getTime()) return -1;
+    if (dateA.getTime() > dateB.getTime()) return 1;
+    return 0;
+  }
+  
+  // Aufgabe hinzufügen
   document.getElementById('add-task-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let title = document.getElementById('task-title').value;
-    let description = document.getElementById('task-description').value;
-    let task = {
-      title: title,
-      description: description,
-      completed: false
-    };
-    addTaskToList(task);
-    clearAddTaskForm();
+    event.preventDefault();
+    let title = document.getElementById('task-title').value;
+    let description = document.getElementById('task-description').value;
+    let dueDate = document.getElementById('task-dueDate').value;
+    let task = {
+      title: title,
+      description: description,
+      dueDate: dueDate, // Hinzufügen eines Fälligkeitsdatums im "YYYY-MM-DD"-Format
+      completed: false
+    };
+  
+    // Aufgabe zur Liste hinzufügen
+    tasks.push(task);
+  
+    // Sortieren der Aufgabenliste nach Fälligkeitsdatum
+    tasks.sort(compareDueDates);
+  
+    // Hier kannst du die Aufgabe zur Liste hinzufügen
+    // Beispiel: Speichern der Aufgabe in einer Datenbank oder im Local Storage
+    addTaskToList(task);
+    clearAddTaskForm();
   });
   
+  // Aufgabenliste anzeigen
   function addTaskToList(task) {
-    let taskList = document.getElementById('tasks');
-    let li = document.createElement('li');
-    li.innerHTML = '<input type="checkbox"> ' + task.title + " / " + task.description;
-    if (task.completed) {
-      li.classList.add('completed');
-    }
-    taskList.appendChild(li);
+    let taskList = document.getElementById('tasks');
+    let li = document.createElement('li');
+    li.innerHTML = '<input type="checkbox"> ' + task.title + '<br>Description: ' + task.description + '<br>Due Date: ' + task.dueDate;
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+    taskList.appendChild(li);
+  
+    // Hier fügen wir die Funktion zur Markierung von Aufgaben hinzu
+    li.querySelector('input[type="checkbox"]').addEventListener('click', function(event) {
+      task.completed = event.target.checked;
+      if (event.target.checked) {
+        li.classList.add('completed');
+      } else {
+        li.classList.remove('completed');
+      }
+    });
   }
   
-  document.getElementById('tasks').addEventListener('change', function(event) {
-    if (event.target.type === 'checkbox') {
-      let li = event.target.parentNode;
-      if (event.target.checked) {
-        li.classList.add('completed');
-      } else {
-        li.classList.remove('completed');
-      }
-    }
+  // Suchfunktion
+  document.getElementById('search-button').addEventListener('click', function() {
+    let searchInput = document.getElementById('search-input').value.toLowerCase();
+    let tasksList = document.getElementById('tasks').getElementsByTagName('li');
+    for (let i = 0; i < tasksList.length; i++) {
+      let taskTitle = tasksList[i].innerText.toLowerCase();
+      if (taskTitle.includes(searchInput)) {
+        tasksList[i].style.display = 'block';
+      } else {
+        tasksList[i].style.display = 'none';
+      }
+    }
   });
   
+  // Hilfsfunktionen / Aufgaben werden geleert
   function clearAddTaskForm() {
-    document.getElementById('task-title').value = '';
-    document.getElementById('task-description').value = '';
+    document.getElementById('task-title').value = '';
+    document.getElementById('task-description').value = '';
+    document.getElementById('task-dueDate').value = '';
   }
-  
